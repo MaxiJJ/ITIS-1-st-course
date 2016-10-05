24ffeafece237da5743f5df4061039379e6df7ca
TheFreaky <maksoh_35@mail.ru>
2016-10-05 22:04:33 +0300

    Added corrections

--- 29.09/Ex3IndexOf.java
+++ 29.09/Ex3IndexOf.java
@@ -2,21 +2,39 @@ class Ex3IndexOf {
     public static void main(String[] args) {
 
         int[] first = {8,236,67,87,23,517,1,6,4,9,87,87,16,6,6,4,8,6,8,236,67,87,23,57,64,
-        	4,9,39,5,981,8,236,67,87,23,57,65861,64,9,4,87,202,8,236,67,87,23,57,293,578,24,47,123,869,2623,4};
-        int[] second = {8,236,67,87,23,57};
-        int len = first.length - 1;
+                4,9,39,5,981,8,236,67,87,23,57,65861,64,9,4,87,202,8,236,67,87,23,57,293,578,24,47,123,869,2623,41741};
+        int[] second = {87,23,57,65861,64};
+        int len = first.length;
         int len2 = second.length;
-        int index = len2 - 1;
 
-        for (int i = 0, k = 1; i <= len; i++) {
+        if (len == 0 || len2 == 0) {
+            System.out.print("Array length must be greater than zero");
+            System.exit(0);
+        }
 
-            if (k == len2) break;
-            if (first[i] == second[0] && !(first[index + k] == second[k])) index = i;
-            if (first[index + k] == second[k]) k++;
+        if (len2 > len) {
+            System.out.print("Second array must be smaller than first.\nOtherwise it never enter");
+            System.exit(0);
+        }
+
+        int k = 0;
+        for (int i = 0; i < len; i++) {
+
+            if (first[i] == second[0]) {
+                for (int j = 0; j < len2; j++) {
+                    if (i+j < len && first[i+j] == second[j]) k++;
+                    else break;
+                }
+                if (k == len2) {
+                    System.out.print("First entry index is: " + i);
+                    System.exit(0);
+                } else {
+                    k = 0;
+                }
+            }
 
         }
 
-        if (index == len2 - 1) System.out.print("Array doesn't enter");
-        else System.out.print("First entry index is: " + index);
+        System.out.print("Array doesn't enter");
     }
 }
