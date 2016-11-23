2284f9e52ce490c34921b1f7f78c3c8a24e4b7a8
TheFreaky <maksoh_35@mail.ru>
2016-11-23 23:14:20 +0300

    Task fixed

--- 17.11/Dynasty.java
+++ 17.11/Dynasty.java
@@ -1,21 +1,26 @@
+import java.util.Scanner;
+
 public class Dynasty {
     public static void main(String[] args) {
-        int startGen = 2;
+        Scanner sc = new Scanner(System.in);
+        int startGen = 1;
         int childInGenCount = 1;
         int multiplier = 10;
         int sum = 0;
 
-        int gen = 5;
-        System.out.println("Sum of children in " + gen + " generation: " 
+        int gen = sc.nextInt();
+        System.out.print("Sum of children in " + gen + " generation: "
                 + countChild(startGen, gen, childInGenCount, multiplier, sum));
     }
 
+
+
     static int countChild(int currentGen, int gen, int childInGenCount, int multiplier, int sum) {
         if (currentGen > gen) return sum;
-
+        if (multiplier < 2) multiplier = 2;
         childInGenCount *= multiplier;
-        multiplier += currentGen % 2 == 1 ? 1 : -2;
+        multiplier += currentGen % 2 == 0 ? 1 : -2;
         sum += childInGenCount;
-        return countChild(currentGen + 1, gen, childInGenCount,multiplier,sum);
+        return countChild(currentGen + 1, gen, childInGenCount, multiplier, sum);
     }
 }
